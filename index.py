5f93a6b54ef23ccd4892e3a127ff71c1c76c0c5a
unknown <syedabdulrehmanlm10@gmail.com>
2024-09-03 14:47:45 +0500

    python lecture 2

---
+++ index.py
@@ -0,0 +1,17 @@
+num1 = 10
+firstinput =   int(input("Please Enter a Number"))
+product1 =  num1 * firstinput
+print("f{num1} x {firstinput} =  product1")
+
+
+num2 = 11
+secondinput =   int(input("Please Enter a Number"))
+product2 =  num2 * secondinput
+print("f{num2} x {secondinput} =  product2")
+
+
+num3 = 12
+thirdinput =   int(input("Please Enter a Number"))
+product3 =  num3 * thirdinput
+print("f{num3} x {thirdinput} =  product3")
+
